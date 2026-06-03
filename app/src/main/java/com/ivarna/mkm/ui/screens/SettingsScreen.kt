2cc02cffbe2b84526020047a822f3831a861b9f5
abhay-byte <abhay02delhi@gmail.com>
2026-06-03 23:50:42 +0530

    fix: update version and date in SettingsScreen

--- app/src/main/java/com/ivarna/mkm/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/ivarna/mkm/ui/screens/SettingsScreen.kt
@@ -132,8 +132,8 @@ fun SettingsScreen(
             item {
                 AppInfoCard(
                     appName = "Minimal Kernel Manager",
-                    version = "v1.3",
-                    buildDate = "Feb 28, 2026"
+                    version = "v1.4",
+                    buildDate = "June 3, 2026"
                 )
             }
 
