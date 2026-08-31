6c224f58d0f47cafb88720171a1caeec3d33c417
abhay-byte <abhay02delhi@gmail.com>
2026-08-31 15:10:02 +0530

    test: host selector tests in activity

--- app/src/androidTest/java/com/ivarna/mkm/ui/components/SelectionBottomSheetTest.kt
+++ app/src/androidTest/java/com/ivarna/mkm/ui/components/SelectionBottomSheetTest.kt
@@ -1,7 +1,8 @@
 package com.ivarna.mkm.ui.components
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.runtime.getValue
@@ -19,7 +20,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class SelectionBottomSheetTest {
     @get:Rule
-    val composeRule = createComposeRule()
+    val composeRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
     fun failedSelectionKeepsSheetOpenAndShowsError() {
